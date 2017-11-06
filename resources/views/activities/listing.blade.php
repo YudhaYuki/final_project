376e0061eb1d5f1eab9f7795caebd4cbe00993e0
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-11-06 22:11:34 +0100

    List activity with images

--- resources/views/activities/listing.blade.php
+++ resources/views/activities/listing.blade.php
@@ -2,38 +2,40 @@
 
 @section('content')
 
-<h1>List of activities</h1>
+<div class="container">
 
-<ul>
-    @foreach($activities as $activity)
-        <li>
-            <a href="{{ route('activity detail', ['id' => $activity->id]) }}">
-                {{ $activity->name }} ({{ $activity->price }})
-            </a>
-        </li>
-    @endforeach
-</ul>
+    <h1>List of activities</h1>
 
-<ul>
     @foreach($activities as $activity)
-        <li>
-            <a href="{{ action('activityController@detail', ['id' => $activity->id]) }}">
-                {{ $activity->name }} ({{ $activity->price }})
-            </a>
-        </li>
-    @endforeach
-</ul>
 
-<ul>
-    @foreach($activities as $activity)
-        <li>
-            <a href="{{ url('activities/activity/' . $activity->id) }}">
-                {{ $activity->name }} ({{ $activity->price }})
-            </a>
-        </li>
+        <div class="row">
+        <div class="col-12">
+
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="{{ asset('uploads/' .$activity->picture) }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title"><a href="{{ route('activity detail', ['id' => $activity->id]) }}">{{ $activity->name }} </a></h4>
+                    <!-- <p class="card-text"><a href="{{ route('activity detail', ['id' => $activity->id]) }}">{{ $activity->description }} </a></p> -->
+                    <a href="{{ route('activity detail', ['id' => $activity->id]) }}" class="btn btn-primary">More details</a>
+                </div>
+            </div>
+
+        </div>
+        </div>
+        <br>
+       
+    <!-- 
+    <li>
+        <a href="{{ route('activity detail', ['id' => $activity->id]) }}">
+            {{ $activity->name }} <img src="{{ asset('uploads/' .$activity->picture) }}"> ({{ $activity->price }})
+        </a>
+    </li> 
+    -->
+    
     @endforeach
-</ul>
 
 @endsection
+
+</div>
 
 @section('page_title') List of activities @endsection
