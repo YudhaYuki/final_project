9c5edf7cfa93adcb8d9ef1258e7ba551424455f2
Ronan <ronan.dineen@webinnovate.ie>
2017-11-07 23:29:23 +0100

    more homepage blade styling

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -55,6 +55,10 @@
                 <a class="nav-link bump-to-left" href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                 </li>
             </ul>
+            
+
+
+
                 @if (Route::has('login'))
                     <div class="top-right links">
                         @auth
@@ -100,6 +104,13 @@
 
 
     @yield('content')
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 </body>
 </html>
