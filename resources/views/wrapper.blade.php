8fb864160d783666cc4f892f72addd114dae570b
Ronan <ronan.dineen@webinnovate.ie>
2017-11-07 13:11:21 +0100

    added logo and navbar styling

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -19,40 +19,42 @@
 </head>
 <body> 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href={{ url('/') }}>BigDayOut</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-    
-        <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-            <a class="nav-link" href="#">How It Works</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">FAQ</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
+<div id="nav-section">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+    <a class="navbar-brand" href={{ url('/') }}><img style="width:100px;" src="/images/test_logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                <a class="nav-link" href="#">How It Works</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">FAQ</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
 
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link bump-to-left" href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            </li>
-        </ul>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link bump-to-left" href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+            </ul>
+        </div>
     </div>
+    </nav>
 </div>
-</nav>
 
 <!-- 
 @if (Route::has('login'))
