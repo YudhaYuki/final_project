e344d236ed448d8e408abac0b31685fcac49c33b
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-11-07 17:23:19 +0100

    renaming the file and class called adventurercontroller into adventurerController

--- app/Http/Controllers/adventurercontroller.php
+++ app/Http/Controllers/adventurercontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class adventurercontroller extends Controller
+class adventurerController extends Controller
 {
     public function listing()
     {
