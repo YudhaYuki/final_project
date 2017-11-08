37d4258b4adc1bc936a8e911fac1f190af7ee09d
Ronan <ronan.dineen@webinnovate.ie>
2017-11-08 23:48:55 +0100

    added styling to listing blade and activity detail blade

--- resources/views/activities/detail.blade.php
+++ resources/views/activities/detail.blade.php
@@ -2,39 +2,131 @@
 
 @section('content')
 
-<br>
+
 <div class="container">
-
-<a href="{{ action('activityController@edit', ['id' => $activity->id]) }}" class="btn btn-primary">Edit ativity</a>
-<br>
-<br>
-
-<!-- Picture :<br> -->
-<p class="picture">
-<img src="{{ asset('uploads/' .$activity->picture) }}">
-</p>
-
-<h1>{{ $activity->name }}</h1>
-
-<!-- Description :<br> -->
-<p class="description">
-{{ $activity->description }}
-</p>
-
-Provider : <br>
-<p class="provider">
-{{ $activity->company_provider }}
-</p>
-
-Price : <br>
-<div class="price">{{ $activity->price }}</div>
-<br>
-
-Activity Category ID :<br>
-<div class="activity_category_id">{{ $activity->activity_category_id }}</div>
-
-
+    <div class="row mt-5">
+        <div class="col-md-6">
+            <img class="img-fluid" src="{{ asset('uploads/' .$activity->picture) }}" alt="activity-picture">
+        </div>
+        <div class="col-md-6">
+            <h1>{{ $activity->name }}</h1>
+            <p class="description">
+                <p><strong>Description</strong></p>
+                    {{ $activity->description }}
+                <p><strong>Provider</strong></p>
+                    {{ $activity->company_provider }}
+                <p><strong>Price</strong></p>
+                    {{ $activity->price }}
+                <p><strong>Activity Category ID</strong></p>
+                    {{ $activity->activity_category_id }}
+            </p><br>
+        </div>
+    </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-2 mb-5 text-center">
+            <a href="{{ action('activityController@edit', ['id' => $activity->id]) }}" class="btn btn-primary bdo-btn-listing">Edit activity</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="incentive">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <span>1</span>
+                    <h4>Cool Destinations</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <span>2</span>
+                    <h4>Quality Activities</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <span>3</span>
+                    <h4>The Best Price</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <span>4</span>
+                    <h4>No Hassle</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                <h5>Praha</h5> 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10240.79816953683!2d14.449798250000002!3d50.0825508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2scz!4v1510090827106" width="250" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <h5>More Info</h5>
+                    <div class="footer-links">
+                        <a href="#">About</a><br>
+                        <a href="#">Blog</a><br>
+                        <a href="#">Sign Your company up</a><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="bdo-why text-center">
+                <h5>Main Menu</h5>
+                <div class="footer-links">
+                    <a href="#">How It Works</a><br>
+                    <a href="#">FAQ</a><br>
+                    <a href="#">Contact</a><br>
+                    <a href="#">Testimonials</a><br>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bdo-why text-center">
+                    <h5>Contact us</h5>
+                    <div class="footer-links">
+                        <a href="#">hello@bigdayout.cz</a><br>
+                        <a href="#">+34 933 930 135</a><br>
+                    </div><br>
+                    <div class="social">
+                    <h5>Follow Us:</h5>
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                    <p>&copy;BigDayOut 2017</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 @endsection
 
 @section('page_title') {{ $activity->name }} ({{ $activity->price }}) @endsection
