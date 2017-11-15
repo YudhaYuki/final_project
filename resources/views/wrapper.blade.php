7b544f449e128403f9c4f637dfcd014e10e82b2c
Andrew Kennedy <andrew.kennedy27@outlook.com>
2017-11-15 15:13:49 +0100

    contact form bootstrap and css

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -9,9 +9,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Permanent+Marker" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Permanent+Marker|Overpass" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link href="{{ asset("css/style.css") }}" rel="stylesheet">
 
     <script
     src="http://code.jquery.com/jquery-3.2.1.min.js"
