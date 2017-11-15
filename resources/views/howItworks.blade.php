857cc43b335cdbfa06b45a338b8066f668af6c9f
Ronan <ronan.dineen@webinnovate.ie>
2017-11-15 15:45:24 +0100

    how it works page changes

--- resources/views/howItworks.blade.php
+++ resources/views/howItworks.blade.php
@@ -3,4 +3,103 @@
 @section('content')
 
 
+
+<div class="howitworks">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bdo-how-head">
+                    <h2>How It Works</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="process-head">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="text-center bdo-process">
+                    <h2>How To Book Your Big Day Out</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="process">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-3 text-center">
+                <div class="process-desc">
+                    <i class="fa fa-users" aria-hidden="true"></i>
+                    <h3>Group</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In eum molestias quaerat labore soluta rerum fuga. Et dolores numquam ipsum vero sed, ipsam, ad error aspernatur tempora deserunt, ducimus blanditiis.</p>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="process-desc">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <h3>Big Day</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In eum molestias quaerat labore soluta rerum fuga. Et dolores numquam ipsum vero sed, ipsam, ad error aspernatur tempora deserunt, ducimus blanditiis.</p>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="process-desc">
+                    <i class="fa fa-ravelry" aria-hidden="true"></i>
+                    <h3>Activities</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In eum molestias quaerat labore soluta rerum fuga. Et dolores numquam ipsum vero sed, ipsam, ad error aspernatur tempora deserunt, ducimus blanditiis.</p>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="process-desc">
+                    <i class="fa fa-user-plus" aria-hidden="true"></i>
+                    <h3>Sign Up</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In eum molestias quaerat labore soluta rerum fuga. Et dolores numquam ipsum vero sed, ipsam, ad error aspernatur tempora deserunt, ducimus blanditiis.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="how-banner mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center mt-5">
+                <div class="bdo-process">
+                    <h2>Surprise!</h2>
+                </div>
+                <p>The day before your bigdayout we will reveal your activity and all the logistics. Sorted!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="fun-people mb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="process-head">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="text-center bdo-process">
+                    <h2>Some of the activities we offer:</h2>
+                    <h4>Have a day you will really remember.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 @endsection
