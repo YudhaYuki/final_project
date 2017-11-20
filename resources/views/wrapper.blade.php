2a685e5fd7aa3dde0c0dbe7343af784dac81d077
Ronan <ronan.dineen@webinnovate.ie>
2017-11-20 18:51:07 +0100

    fix login bug and some styling to wrapper (permission status labels)

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -13,6 +13,8 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset("css/style.css") }}" rel="stylesheet">
+    
+
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -22,7 +24,6 @@
     $( "#datepicker" ).datepicker();
     } );
   </script>
-    
     
 </head>
 <body> 
@@ -49,22 +50,28 @@
                 </li>
                 
                 @if(Auth::check() && Auth::user()->isManagers())
+                <div class="bdo-login-permission">
                     <li class="nav-item">
-                        Manager!!!
-                    </li>    
+                        Manager
+                    </li>  
+                </div>  
                 @endif
 
 
                 @if(Auth::check() && Auth::user()->isEmployees())
+                <div class="bdo-login-permission">
                     <li class="nav-item">
-                        Employees!!!
-                    </li>    
+                        Employee
+                    </li>   
+                </div> 
                 @endif
 
                 @if(Auth::check() && Auth::user()->isTraveler())
+                <div class="bdo-login-permission">
                     <li class="nav-item">
-                        Travel!!!
+                        Traveler
                     </li>    
+                </div>
                 @endif
             </ul>
 
@@ -243,11 +250,11 @@
     @yield('content')
 
 
-<!--
+
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<script src="js/custom.js"></script> -->
+<script src="js/custom.js"></script> 
 
 </body>
 </html>
