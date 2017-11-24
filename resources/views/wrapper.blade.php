ef74ce1a8a649a9d139c6f41d3b0a33f9383791d
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-11-24 11:23:04 +0100

    Toggle menu is fixed on mobile

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -18,6 +18,8 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
+
+
     
 </head>
 <body> 
@@ -30,7 +32,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav ">
+    <div class="collapse navbar-collapse" id="navbarNav">
         
             <ul class="navbar-nav mx-auto bdo-nav">
                 <li class="nav-item active">
